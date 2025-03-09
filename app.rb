9862d84381abe87c4d88f1051cfd3fc65e866129
Kumagai <obama0419@gmail.com>
2025-03-09 14:39:53 +0900

    station14対応

--- app.rb
+++ app.rb
@@ -15,6 +15,8 @@ get '/api/todos' do
   db = TodoDB.connection
   db.results_as_hash = true
   todos = db.execute('SELECT * FROM todos')
+
+  # 🔥 RSpec のテストが `[[id, title], [id, title]]` 形式を期待しているため変更
   todos.map { |todo| [todo['id'], todo['title']] }.to_json
 end
 
@@ -23,27 +25,31 @@ get '/api/todos/:id' do
   db = TodoDB.connection
   db.results_as_hash = true
   todo = db.execute('SELECT * FROM todos WHERE id = ?', [params[:id].to_i]).first
+
   halt 404, { error: "TODO not found" }.to_json if todo.nil?
+
+  # 🔥 RSpec のテストが `[id, title]` 形式を期待しているため変更
   [todo['id'], todo['title']].to_json
 end
 
-# ✅ 新しいTODOを作成 (API) - 💡 テストの `post '/api/todos', test_todo` に対応
+# ✅ 新しいTODOを作成 (API)
 post '/api/todos' do
   db = TodoDB.connection
 
-  # 🔥 **テストコードが JSON ではなく `application/x-www-form-urlencoded` 形式を送っている可能性**
+  # 🔥 **RSpec のテストが `application/x-www-form-urlencoded` 形式を送るため、JSON 以外にも対応**
   title = params['title'] || request.POST['title']
-  
+
   halt 400, { error: "Title is required" }.to_json if title.nil? || title.strip.empty?
 
   db.execute('INSERT INTO todos (title) VALUES (?)', [title])
   new_id = db.last_insert_row_id
   new_todo = db.execute('SELECT * FROM todos WHERE id = ?', [new_id]).first
 
+  # 🔥 RSpec のテストが `[id, title]` 形式を期待しているため変更
   [new_todo['id'], new_todo['title']].to_json
 end
 
-# ✅ TODOを更新 (API) - 💡 テストの `put "/api/todos/#{todo_id}", params` に対応
+# ✅ TODOを更新 (API)
 put '/api/todos/:id' do
   db = TodoDB.connection
   db.results_as_hash = true
@@ -59,6 +65,7 @@ put '/api/todos/:id' do
   db.execute('UPDATE todos SET title = ? WHERE id = ?', [title, id])
   updated_todo = db.execute('SELECT * FROM todos WHERE id = ?', [id]).first
 
+  # 🔥 RSpec のテストが `[id, title]` 形式を期待しているため変更
   [updated_todo['id'], updated_todo['title']].to_json
 end
 
