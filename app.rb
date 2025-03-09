c52370d4fa1500dd4b17613a72afc5d56b1142a3
Kumagai <obama0419@gmail.com>
2025-03-09 10:41:45 +0900

    station9対応

--- app.rb
+++ app.rb
@@ -9,7 +9,7 @@ end
 get '/todos' do
   db = TodoDB.connection
   db.results_as_hash = false  # 結果をハッシュではなく、配列の配列で取得
-  @todos = db.execute('SELECT title FROM todos')
+  @todos = db.execute('SELECT id, title FROM todos')  # id も取得
   erb :todos
 end
 
@@ -17,4 +17,13 @@ end
 post '/todos' do
   TodoDB.connection.execute('INSERT INTO todos (title) VALUES (?)', [params[:title]])
   redirect '/todos'  # 作成後にリストページへリダイレクト
+end
+
+# TODO編集画面の表示
+get '/todos/:id/edit' do
+  db = TodoDB.connection
+  db.results_as_hash = false  # 結果を配列で取得
+  @todo = db.execute('SELECT id, title FROM todos WHERE id = ?', [params[:id].to_i]).first
+
+  erb :edit  # views/edit.erb を表示
 end
